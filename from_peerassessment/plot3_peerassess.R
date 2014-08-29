54681ddb1385990195252633c6c8f07da6178e3d
Iwunze, Ogonna <ogonna.iwunze@bskyb.com>
2014-08-29 07:46:59 +0100

    improvements from peer assessment

---
+++ from_peerassessment/plot3_peerassess.R
@@ -0,0 +1,25 @@
+source("downloadArchive.R")
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot3.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
+
